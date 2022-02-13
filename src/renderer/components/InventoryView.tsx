47a4a5e44a449b46621a623d4a6cece8f003737e
Mason Jennings <mminute@users.noreply.github.com>
2022-02-13 10:05:15 -0800

    Update to sort item order by column

--- src/renderer/components/InventoryView.tsx
+++ src/renderer/components/InventoryView.tsx
@@ -1,10 +1,15 @@
-import { Box, Table, TapArea, Text } from 'gestalt';
+import { useState } from 'react';
+import { Box, Table, Text } from 'gestalt';
 import InventoryItem from '../../Inventory/InventoryItem';
+
+const ASC = 'asc';
+const DESC = 'desc';
 
 const inventoryItemAttributes = Object.getOwnPropertyNames(
   new InventoryItem({ id: '', name: '', archived: false, quantity: 1 })
 ).filter(
   (attr) =>
+    // Do not show these item attributes
     ![
       'id',
       'archived',
@@ -27,11 +32,25 @@ function camelCaseToSentence(camelCasedStr: string) {
   );
 }
 
-function HeaderCell({ txt }: { txt: string }) {
+function HeaderCell({
+  attr,
+  onSortChange,
+  sortCol,
+  sortOrder,
+}: {
+  attr: string;
+  onSortChange: (col: string) => void;
+  sortCol: string;
+  sortOrder: 'asc' | 'desc';
+}) {
   return (
-    <Table.HeaderCell>
-      <Text weight="bold">{txt}</Text>
-    </Table.HeaderCell>
+    <Table.SortableHeaderCell
+      onSortChange={() => onSortChange(attr)}
+      sortOrder={sortOrder}
+      status={sortCol === attr ? 'active' : 'inactive'}
+    >
+      <Text weight="bold">{camelCaseToSentence(attr)}</Text>
+    </Table.SortableHeaderCell>
   );
 }
 
@@ -57,6 +76,46 @@ function Row({
   );
 }
 
+function sortAndFilter({
+  inventory,
+  showArchived,
+  sortCol,
+  sortOrder,
+}: {
+  inventory: Array<InventoryItem>;
+  showArchived: boolean;
+  sortCol: string;
+  sortOrder: 'asc' | 'desc';
+}): Array<InventoryItem> {
+  const sortOrderValue = sortOrder === ASC ? -1 : 1;
+
+  const items = showArchived
+    ? inventory
+    : inventory.filter((itm) => !itm.archived);
+
+  items.sort((firstItem, secondItem) => {
+    let firstItemVal = firstItem[sortCol];
+    let secondItemVal = secondItem[sortCol];
+
+    if (typeof firstItemVal === 'string') {
+      firstItemVal = firstItemVal.toLowerCase();
+      secondItemVal = secondItemVal.toLowerCase();
+    }
+
+    if (firstItemVal < secondItemVal) {
+      return -sortOrderValue;
+    }
+
+    if (firstItemVal > secondItemVal) {
+      return sortOrderValue;
+    }
+
+    return 0;
+  });
+
+  return items;
+}
+
 interface Props {
   inventory: Array<InventoryItem>;
   onSelectItem: (item: InventoryItem | null) => void;
@@ -64,9 +123,19 @@ interface Props {
 }
 
 const InventoryView = ({ inventory, onSelectItem, showArchived }: Props) => {
-  const items = showArchived
-    ? inventory
-    : inventory.filter((itm) => !itm.archived);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>(DESC);
+  const [sortCol, setSortCol] = useState(inventoryItemAttributes[0]);
+
+  const onSortChange = (col: string) => {
+    if (sortCol !== col) {
+      setSortCol(col);
+      setSortOrder(DESC);
+    } else {
+      setSortOrder(sortOrder === ASC ? DESC : ASC);
+    }
+  };
+
+  const items = sortAndFilter({ inventory, showArchived, sortCol, sortOrder });
 
   return (
     <Box width="100%" paddingX={4}>
@@ -76,7 +145,10 @@ const InventoryView = ({ inventory, onSelectItem, showArchived }: Props) => {
             {inventoryItemAttributes.map((attr) => (
               <HeaderCell
                 key={`header-${attr}`}
-                txt={camelCaseToSentence(attr)}
+                attr={attr}
+                onSortChange={onSortChange}
+                sortCol={sortCol}
+                sortOrder={sortOrder}
               />
             ))}
           </Table.Row>
